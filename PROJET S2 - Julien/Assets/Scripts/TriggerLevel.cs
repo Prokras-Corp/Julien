cbb2ae16a2133ce99f91e463be2d8f2e69e67839
Julien Auzanneau <julien.auzanneau@epita.fr>
2021-04-22 16:28:41 +0200

    avec niveaux

---
+++ PROJET S2 - Julien/Assets/Scripts/TriggerLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TriggerLevel : MonoBehaviour
+{
+    [SerializeField] GameObject map;
+    [SerializeField] GameObject toActivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        map.SetActive(false);
+        toActivate.SetActive(true);
+    }
+}
