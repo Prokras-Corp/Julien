95ddaa25d9e61257c5c636482d776a86290658df
Julien Auzanneau <julien.auzanneau@epita.fr>
2021-04-26 18:47:54 +0200

    AI

---
+++ PROJET S2 - Julien/Assets/Scripts/TriggerVillage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerVillage : MonoBehaviour
+{
+    CharacterController player;
+
+    [SerializeField] GameObject level;
+    [SerializeField] GameObject village;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        level.SetActive(false);
+        village.SetActive(true);
+
+        player = other.GetComponent<CharacterController>();
+        player.enabled = false;
+        player.transform.position = new Vector3(0, 0, 0);
+        player.enabled = true;
+    }
+}
---
+++ PROJET S2 - Julien/Assets/nav.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class nav : MonoBehaviour
+{
+    /*[SerializeField]*/ GameObject target;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] float DistanceSociale;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 position = agent.transform.position;
+        Vector3 destination = target.transform.position;
+
+        agent.SetDestination(destination);
+
+        if (Vector3.Distance(position, destination) < DistanceSociale)
+        {
+            agent.SetDestination(position);
+        }
+    }
+}
